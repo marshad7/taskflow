7e7dd4197565ac9ec1c267bdddfc3012bc9afcac
Mahad Arshad <mahad.arshad111@gmail.com>
2026-01-26 23:22:14 +0400

    Wire Zod validation into tasks routes

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -6,14 +6,30 @@ const {
   updateTask,
   deleteTask,
 } = require("../controllers/tasks.controller");
+const { validate } = require("../middleware/validate");
+const {
+  createTaskSchema,
+  updateTaskSchema,
+} = require("../validation/tasks.schemas");
 
 const router = express.Router();
 
 router.use(requireAuth);
 
 router.get("/", listTasks);
-router.post("/", createTask);
-router.put("/:id", updateTask);
+
+router.post(
+  "/",
+  validate(createTaskSchema),
+  createTask
+);
+
+router.put(
+  "/:id",
+  validate(updateTaskSchema),
+  updateTask
+);
+
 router.delete("/:id", deleteTask);
 
 module.exports = { tasksRouter: router };
